96e9f20b702910b619920b5854e48fc94e9ef01d
sssgordon <gso7@uwo.ca>
2019-12-12 01:49:54 +0100

    Landing page css done

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,17 +3,19 @@ import GoogleLoginButton from "../GoogleLoginButton";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import outcryWhite from "../../assets/outcry-white.png";
+import editUserDetails from "../../assets/edit-user-details-white.png";
+import editAlias from "../../assets/edit-alias-white.png";
 import "./Landing.css";
 
 class Landing extends Component {
   render() {
     const { user } = this.props;
 
-    const detailsLink = (
-      <Link to="/details" style={{ textDecoration: "none" }}>
-        <p className={`${user.email ? "fade-in" : "hidden"}`}>Details</p>
-      </Link>
-    );
+    // const detailsLink = (
+    //   <Link to="/details" style={{ textDecoration: "none" }}>
+    //     <p className={`${user.email ? "fade-in" : "hidden"}`}>Details</p>
+    //   </Link>
+    // );
     // const aliasLink = user.email && (
     //   <Link to="/alias">
     //     <p>Alias</p>
@@ -25,12 +27,30 @@ class Landing extends Component {
         <div id="landing-wrapper-parent">
           <div id="landing-wrapper">
             <img id="outcry" src={outcryWhite} alt="title" />
+            <Link to="/details">
+              <img
+                className={`${
+                  user.email ? "edit-user-details" : "edit-user-details-hidden"
+                }`}
+                src={editUserDetails}
+                alt="edit-user-details"
+              />
+            </Link>
             <div id="login-btn-wrapper">
               <p>登入帳號：</p>
               <GoogleLoginButton />
             </div>
+            <Link to="/alias">
+              <img
+                className={`${
+                  user.email ? "edit-user-alias" : "edit-user-alias-hidden"
+                }`}
+                src={editAlias}
+                alt="edit-user-alias"
+              />
+            </Link>
           </div>
-          {detailsLink}
+          {/* {detailsLink} */}
           {/* {aliasLink} */}
         </div>
       </Fragment>

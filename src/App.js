8a7916f2952a82ca18eabc5ca597bb127c7d40a0
sssgordon <gso7@uwo.ca>
2019-12-12 22:42:23 +0100

    Add fallback image in case video is not working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AliasFormContainer from "./components/AliasForm";
 import DetailsFormContainer from "./components/DetailsForm";
 import { Route } from "react-router-dom";
 import Landing from "./components/Landing/Landing";
+import alternativeBackgroundImage from "./assets/test-background.jpg";
 import backgroundVideo from "./assets/sham-shui-po-bg-vid-2.mp4";
 import "./App.css";
 
@@ -13,7 +14,7 @@ class App extends Component {
       <div id="background">
         <video id="bg-vid" loop autoPlay muted preload="metadata">
           <source src={backgroundVideo} type="video/mp4" />
-          Your browser does not support the video tag.
+          <img src={alternativeBackgroundImage} alt="fallback image" />
         </video>
         <div id="background-overlay">
           <Route exact path="/details" component={DetailsFormContainer} />

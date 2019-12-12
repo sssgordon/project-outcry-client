8daf602ff866edab7408994e3e318892d3f3f1e5
sssgordon <gso7@uwo.ca>
2019-12-12 16:10:10 +0100

    Video works 2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import AliasFormContainer from "./components/AliasForm";
 import DetailsFormContainer from "./components/DetailsForm";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Landing from "./components/Landing/Landing";
+import backgroundVideo from "./assets/sham-shui-po-bg-vid-2.mp4";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div id="background">
+        <video id="bg-vid" loop autoPlay muted preload="metadata">
+          <source src={backgroundVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
         <div id="background-overlay">
           <Route exact path="/details" component={DetailsFormContainer} />
           <Route exact path="/alias" component={AliasFormContainer} />
